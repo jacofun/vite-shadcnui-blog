352d534a43e061ab5f47a171f06c0aedfeb15615
JACO <97010826+jacofun@users.noreply.github.com>
2026-08-26 10:22:48 +0800

    移除页面切换渐变并统一底色

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-﻿
-import { AnimatePresence, motion } from "framer-motion";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
 import Footer from "./components/common/Footer";
 
 function App() {
-  const location = useLocation();
   return (
-    <>
-      <AnimatePresence mode='wait'>
-        <motion.div
-          key={location.pathname}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.4 }}
-        >
-          <Outlet /> {/* 子页面会在这里渲染 */}
-        </motion.div>
-      </AnimatePresence>
+    <div className="min-h-screen bg-[#070a12]">
+      <Outlet />
       <Footer />
-    </>
-
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
