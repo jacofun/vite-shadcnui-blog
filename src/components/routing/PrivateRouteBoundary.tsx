3b245b7cff95bc3b1f84cca336d28ef24c102cbc
JACO <97010826+jacofun@users.noreply.github.com>
2026-09-06 22:32:55 +0800

    接入私人资源性能诊断

--- src/components/routing/PrivateRouteBoundary.tsx
+++ src/components/routing/PrivateRouteBoundary.tsx
@@ -1,11 +1,13 @@
 import type { JSX } from "react";
 import { Outlet } from "react-router-dom";
 
+import PrivatePerformanceObserver from "@/components/resources/PrivatePerformanceObserver";
 import { PrivateAuthProvider } from "@/contexts/PrivateAuthContext";
 
 export default function PrivateRouteBoundary(): JSX.Element {
   return (
     <PrivateAuthProvider>
+      <PrivatePerformanceObserver />
       <Outlet />
     </PrivateAuthProvider>
   );
